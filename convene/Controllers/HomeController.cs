268f966b728804509e1ab79c1633a562db597ee5
Chris Weldon <chris@chrisweldon.net>
2013-05-09 09:32:46 -0500

    Adding HomeController with initial index view.

---
+++ convene/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using convene.Models;
+
+namespace convene.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
